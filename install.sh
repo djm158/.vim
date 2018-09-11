1eff60b77ea0a7c68019df811808511e0d2142cf
Dan McGrath <daniel.mcgrath9@gmail.com>
2018-09-11 16:37:00 -0400

    adding .tmux.conf for now

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 ln -s ~/.vim/.vimrc ~/.vimrc
+ln -s ~/.vim/.tmux.conf ~/.tmux.conf
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
