aadac96eab1f59e4aa474a7ed8eb26e49e5490c1
Dan McGrath <daniel.mcgrath9@gmail.com>
2018-09-13 17:02:50 -0400

    update install to add ctags and global gitignore

--- install.sh
+++ install.sh
@@ -2,6 +2,9 @@
 
 ln -s ~/.vim/.vimrc ~/.vimrc
 ln -s ~/.vim/.tmux.conf ~/.tmux.conf
+ln -s ~/.vim/.ctags ~/.ctags
+ln -s ~/.vim/.gitignore_global ~/.gitignore_global
+
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
